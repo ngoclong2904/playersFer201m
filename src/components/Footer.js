8b893ed5cafa70c65208faa8b82f67525aaba416
Long Le <capngoclong@gmail.com>
2023-06-03 03:37:31 +0700

    Component Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul className="navigation-ul-2">
+            <div className="card2"> Copyright &copy; 2022</div>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
