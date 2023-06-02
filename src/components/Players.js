4b8613af9374b9c97044dd0c1142afb3e39834d7
Long Le <capngoclong@gmail.com>
2023-06-03 03:54:55 +0700

    State Hook

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,20 +1,39 @@
 import React from "react";
 import { Players } from "../shared/ListOfPlayers";
+import { useState } from "react";
 export default function PlayersVN() {
+  const [player, setPlayer] = useState([]);
   return (
     <div className="container">
       {Players.map((player) => (
-        <div className="column">
+        <div className="column" key={player.id}>
           <div className="card">
-            <img src={player.img} />
+            <img src={player.img} alt="" />
             <h3>{player.name}</h3>
-            <p className="title">{player.club}</p>
-            <p>
-              <button>Detail</button>
-            </p>
+            <p className="title"></p>
+            {/* <p><button onClick={()=>{setPlayer(player)}}><a href='#popup1' id='openPopUp'>Detail</a></button></p> */}
+            <button
+              onClick={() => {
+                setPlayer(player);
+              }}
+            >
+              <a href="#popup1" id="openPopUp">
+                Detail
+              </a>
+            </button>
           </div>
         </div>
       ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/PlayersPresentation.js
+++ src/components/PlayersPresentation.js
@@ -1,20 +1,39 @@
 import React from "react";
-export default function PlayersPresentation({ players }) {
-  console.log(players);
+import { useState } from "react";
+import { Players } from "../shared/ListOfPlayers";
+export default function PlayersVN() {
+  const [player, setPlayer] = useState([]);
   return (
     <div className="container">
-      {players.map((player) => (
-        <div className="column">
+      {Players.map((player) => (
+        <div className="column" key={player.id}>
           <div className="card">
-            <img src={player.img} />
+            <img src={player.img} alt="" />
             <h3>{player.name}</h3>
-            <p className="title">{player.club}</p>
-            <p>
-              <button>Detail</button>
-            </p>
+            <p className="title"></p>
+            {/* <p><button onClick={()=>{setPlayer(player)}}><a href='#popup1' id='openPopUp'>Detail</a></button></p> */}
+            <button
+              onClick={() => {
+                setPlayer(player);
+              }}
+            >
+              <a href="#popup1" id="openPopUp">
+                Detail
+              </a>
+            </button>
           </div>
         </div>
       ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
   );
 }
