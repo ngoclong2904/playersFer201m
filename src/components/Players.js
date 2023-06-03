0569bbbcff8574cdab39759d653186487b4fac83
capngoclong <capngoclong@gmail.com>
2023-06-03 16:01:10 +0700

    Component part 1

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,20 +1,58 @@
 import React from "react";
-import { Players } from "../shared/ListOfPlayers";
-export default function PlayersVN() {
-  return (
-    <div className="container">
-      {Players.map((player) => (
+import { Component } from "@angular/core";
+export default class PlayersVN extends Component {
+  render() {
+    return (
+      <div className="container">
         <div className="column">
           <div className="card">
-            <img src={player.img} />
-            <h3>{player.name}</h3>
-            <p className="title">{player.club}</p>
-            <p>
-              <button>Detail</button>
-            </p>
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
           </div>
         </div>
-      ))}
-    </div>
-  );
+        <div className="column">
+          <div className="card">
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <img src="assets/images/cr.jpg"/>
+            <h3>Cristiano Ronaldo</h3>
+            <p className="title">Manchester United</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
